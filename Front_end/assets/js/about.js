881f99c0ca1b9d1a35c2a401c7187f1c99d36c23
Daviddoan14122k <doanchuong14122k@gmail.com>
2021-10-28 22:55:45 +0700

    update menu responsive

--- Front_end/assets/js/about.js
+++ Front_end/assets/js/about.js
@@ -1,3 +1,17 @@
+// menuResponsive
+var menuResponsive = {
+  init: function() {
+      this.toggleMenu()
+  },
+  toggleMenu: function() {
+      var button = document.querySelector('.header__menu-bar');
+      var menu = document.querySelector('.header__menu-list');
+      button.addEventListener('click', function() {
+          menu.classList.toggle('show')
+      })
+  }
+}
+menuResponsive.init()
 
 new WOW().init();
 $('.reset').click(function(){
